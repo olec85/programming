0809451812eda39ea5d34030cd1a547e3dafe08b
olec85 <85439166+olec85@users.noreply.github.com>
2022-03-26 16:15:30 +0300

    add js

---
+++ main.js
@@ -0,0 +1,17 @@
+//var x = 2 + 5 * 8;
+
+//console.log(x);
+//домашнее задание 1
+var TemC = prompt('Введите температуру по Цельсию');
+
+  var TemF = (9 / 5) * TemC + 32;
+
+  console.log('Температура по Фаренгейту ' + TemF);
+  //домашнее задание 2
+  var name = "Василий";
+
+  var admin;
+
+  admin = name;
+
+  console.log(admin);
