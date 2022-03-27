78b5374691fe162412b6120a31987509398c3aca
olec85 <85439166+olec85@users.noreply.github.com>
2022-03-27 08:57:36 +0300

    add js

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 //var x = 2 + 5 * 8;
 
 //console.log(x);
-//домашнее задание 1
+//Домашнее задание первого урока
+//часть 1
 var TemC = prompt('Введите температуру по Цельсию');
 
   var TemF = (9 / 5) * TemC + 32;
 
   console.log('Температура по Фаренгейту ' + TemF);
-  //домашнее задание 2
+  //часть 2
   var name = "Василий";
 
   var admin;
@@ -15,3 +16,9 @@ var TemC = prompt('Введите температуру по Цельсию');
   admin = name;
 
   console.log(admin);
+//часть 3
+  var x = 1000;
+
+  var y = "108"
+
+  console.log(x + y)
